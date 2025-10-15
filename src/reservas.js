c0e24ab9456dae6532036ca32053cb2b46b1e618
jesicalopez <jesicaplopez35@gmail.com>
2025-10-15 18:15:07 -0300

    usuarios

--- src/reservas.js
+++ src/reservas.js
@@ -7,10 +7,13 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/salones', v1SalonesRutas);
+// app.use('/api/v1/usuarios', v1SalonesRutas)
+
+
 // app.use('/api/v1/salones', apicache( '5 minutos'), v1SalonesRutas) ejemplo apicache
 
 // app.use('/api/v1/turnos', v1SalonesRutas)
-// app.use('/api/v1/usuarios', v1SalonesRutas)
+
 // app.use('/api/v1/reservas', v1SalonesRutas)
 
 
