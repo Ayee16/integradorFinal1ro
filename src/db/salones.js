122e0682a319d6b3a9e9c8b70d17cf2f8bf6828f
jesicalopez <jesicaplopez35@gmail.com>
2025-10-15 17:57:41 -0300

    buenas practicas

---
+++ src/db/salones.js
@@ -0,0 +1,51 @@
+import { conexion } from "./conexion.js";
+
+export default class Salones {
+    buscarTodos = async() =>{
+        const sql = 'SELECT * FROM salones WHERE activo = 1';      
+        const [salones] = await conexion.execute(sql);
+        return salones;
+    }
+
+    buscarPorId = async(salon_id) =>{
+        const sql = 'SELECT * FROM salones WHERE activo = 1 AND salon_id = ?';
+        const [salon] = await conexion.execute(sql, [salon_id]);
+        if (salon.length === '0'){
+            return null
+        }
+        return salon[0];
+    }
+
+    modificar = async(salon_id,datos) => {
+
+        const camposAActualizar = Object.keys(datos); //obtengo claves
+        const valoresAActualizar = Object.values(datos); //obtengo valores
+        const setValores = camposAActualizar.map(campo => `${campo} = ?`).join(', '); //junto claves y le agrego ?
+        const parametros = [...valoresAActualizar,salon_id]; //junto valores y salon id y lo meto en const parametros
+        const sql = `UPDATE salones SET ${setValores} WHERE salon_id = ?`; //consulta sql
+        const [result] = await conexion.execute(sql,parametros);
+        if (result.affectedRows ===0){
+            return null;
+        }
+        return this.buscarPorId(salon_id);
+    }
+    
+    crear = async(salon) =>{
+            const {titulo, direccion, capacidad, importe} = salon;
+            const sql = 'INSERT INTO salones (titulo, direccion, capacidad, importe) VALUES (?,?,?,?)' ;
+            const [result] = await conexion.execute(sql,[titulo, direccion, capacidad, importe]);
+            if (result.affectedRows == 0){
+                return null //en capa de datos no se retorna informacion solo retorna datos puros
+            }
+            return this.buscarPorId(result.insertId);
+        }
+
+    eliminar = async(salon_id) =>{
+        const sql = 'UPDATE salones SET activo = 0 WHERE salon_id = ?'
+        const [result] = await conexion.execute(sql, [salon_id]);
+        if (result.affectedRows === 0) {
+            return null;
+        }
+        return true; // o podrías retornar el salon_id eliminado
+    }
+}
